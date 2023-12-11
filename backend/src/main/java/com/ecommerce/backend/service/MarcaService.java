4a56a7c8079bbcaf2d3aa26dd99bae8bc4885b0a
Matheus <matheusmeneses07@gmail.com>
2023-12-11 08:18:08 -0300

    marca-crud

--- backend/src/main/java/com/ecommerce/backend/service/MarcaService.java
+++ backend/src/main/java/com/ecommerce/backend/service/MarcaService.java
@@ -4,11 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ecommerce.backend.entity.Marca;
 import com.ecommerce.backend.repository.MarcaRepository;
 
+@Service
 public class MarcaService {
+    
     @Autowired
     private MarcaRepository marcaRepository;
 
