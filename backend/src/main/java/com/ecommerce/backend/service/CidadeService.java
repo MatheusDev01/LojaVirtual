53be71eff2f5c900a513f553bc2fc5084d4f8cfc
Matheus <matheusmeneses07@gmail.com>
2023-12-11 22:37:13 -0300

    cidade-crud

---
+++ backend/src/main/java/com/ecommerce/backend/service/CidadeService.java
@@ -0,0 +1,34 @@
+package com.ecommerce.backend.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.backend.entity.Cidade;
+import com.ecommerce.backend.repository.CidadeRepository;
+
+@Service
+public class CidadeService {
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    public List<Cidade> buscarTodos(){
+        return cidadeRepository.findAll();
+    }
+
+    public Cidade inserir(Cidade cidade){
+        cidade.setDataCriacao(new Date());
+        Cidade cidadeNova = cidadeRepository.saveAndFlush(cidade);
+        return cidadeNova;
+    }
+    public Cidade alterar(Cidade cidade){
+        cidade.setDataAtualizacao(new Date());
+        return cidadeRepository.saveAndFlush(cidade);
+    }
+    public void excluir(Long id){
+        Cidade cidade = cidadeRepository.findById(id).get();
+        cidadeRepository.delete(cidade);
+    }
+}
