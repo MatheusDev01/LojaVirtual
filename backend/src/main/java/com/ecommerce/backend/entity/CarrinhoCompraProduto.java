ee8cc01d03cdedcad029728d168b405313c57d0e
Matheus <matheusmeneses07@gmail.com>
2023-12-20 21:15:47 -0300

    CarrinhoCompra

--- backend/src/main/java/com/ecommerce/backend/entity/CarrinhoCompraProduto.java
+++ backend/src/main/java/com/ecommerce/backend/entity/CarrinhoCompraProduto.java
@@ -2,10 +2,14 @@ package com.ecommerce.backend.entity;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -22,6 +26,15 @@ public class CarrinhoCompraProduto {
     private Double valor;
     private Double quantidade;
     private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "idCarrinhoCompra")
+    @JsonIgnore
+    private CarrinhoCompra carrinhoCompra;
+
+    @ManyToOne
+    @JoinColumn(name = "idProduto")
+    private Produto produto;
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
